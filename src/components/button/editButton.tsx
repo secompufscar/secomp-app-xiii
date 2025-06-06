f88dbd4e6d968d4a6bd45590ac311c108fda9463
shinoyori <aylinha2709@gmail.com>
2025-06-06 16:09:49 -0300

    feat: added admin and user profile pages

---
+++ src/components/button/editButton.tsx
@@ -0,0 +1,23 @@
+import { ParamListBase, useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { Pressable, View } from "react-native";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faPen } from "@fortawesome/free-solid-svg-icons";
+import { colors } from "../../styles/colors";
+
+export default function EditButton(){
+    const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
+
+    return(
+        <Pressable
+        className="w-[32px] h-[32px] mt-16 mb-10"
+          //onPress={() => }
+        >
+            {({ pressed }) => (
+                <View className={`flex items-center justify-center p-2 rounded-[4px] ${pressed ? "bg-[#29303F]/80" : "bg-[#29303F]"}`}>
+                    <FontAwesomeIcon icon={faPen} size={16} color={colors.blue[200]} />
+                </View>
+            )}
+        </Pressable>
+    );
+}
