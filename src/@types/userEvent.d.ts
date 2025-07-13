e419996a78bb020b3d91be5fee3e917f78ab6fc1
Enzo Murayama <enzomurayama@gmail.com>
2025-07-13 02:44:28 -0300

    feat: events services

---
+++ src/@types/userEvent.d.ts
@@ -0,0 +1,13 @@
+export enum UserEventStatus {
+  Cancelado = 0,
+  Inscrito = 1,
+  Fechado = 2,
+}
+
+interface UserEvent {
+  id: string;
+  userId: string;
+  eventId: string;
+  status: UserEventStatus;
+  createdAt: Date;
+}
