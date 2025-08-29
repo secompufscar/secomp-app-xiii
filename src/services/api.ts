00bd84353011f2cf696af5c8b9ba70985a3d55b6
Enzo Murayama <enzomurayama@gmail.com>
2025-08-29 16:20:34 -0300

    refactor: api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,32 +1,28 @@
 import axios from "axios";
-
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const getApi = () => {
-  const api = axios.create({
-    baseURL: "https://api.secompufscar.com.br/api/v1",
-  });
 
-  api.interceptors.request.use(async (config) => {
+const api = axios.create({ baseURL: "https://api.secompufscar.com.br/api/v1", });
+
+// Interceptor de Requisição: Adiciona o token em todas as chamadas
+api.interceptors.request.use(
+  async (config) => {
     try {
-      // Obtém o token do usuário através do asyncStorage (token criado no Login em 'user.ts login()' )
       const userToken = await AsyncStorage.getItem("userToken");
 
-      if (typeof userToken == "string" && userToken.trim() != "") {
-        config.headers["Authorization"] = `Bearer ${userToken}`;
-      } else {
-        console.log("Erro na obtenção do token do usuário");
+      if (userToken) {
+        config.headers.Authorization = `Bearer ${userToken}`;
       }
 
       return config;
     } catch (error) {
-      console.log(error);
-    } finally {
-      return config;
+      console.error("Erro no interceptor de requisição ao buscar token:", error);
+      return Promise.reject(error);
     }
-  });
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
-  return api;
-};
-
-export default getApi();
+export default api;
